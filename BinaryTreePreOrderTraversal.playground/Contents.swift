86e2bec7564bb853334e047a8009c2464ad68c5f
Jatinder Pal Singh Khera <jatinderpalsinghkhera@Jatinders-MacBook-Air.local>
2019-09-17 16:44:27 +0100

    Binary tree pre-order traversal

---
+++ BinaryTreePreOrderTraversal.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+ // Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+    self.val = val
+    self.left = nil
+    self.right = nil
+ }
+}
+
+class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        guard let node = root else {
+            return [Int]()
+        }
+        
+        var output = [Int]()
+        output.append(node.val)
+        if node.left != nil {
+            var left = preorderTraversal(node.left)
+            output.append(contentsOf: left)
+        }
+        if node.right != nil {
+            var right = preorderTraversal(node.right)
+            output.append(contentsOf: right)
+        }
+        
+        return output
+    }
+}
