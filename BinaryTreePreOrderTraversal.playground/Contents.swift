c858749f819c45386df5854cbbba97998580f8fc
Jatinder Pal Singh Khera <jatinderpalsinghkhera@Jatinders-MacBook-Air.local>
2019-09-18 11:28:27 +0100

    Counting Valleys

--- BinaryTreePreOrderTraversal.playground/Contents.swift
+++ BinaryTreePreOrderTraversal.playground/Contents.swift
@@ -12,6 +12,30 @@ public class TreeNode {
  }
 }
 
+// Recursion (poor on memory), suggest using inout param and a seconf function
+//called from first
+//class Solution {
+//    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+//        guard let node = root else {
+//            return [Int]()
+//        }
+//
+//        var output = [Int]()
+//        output.append(node.val)
+//        if node.left != nil {
+//            var left = preorderTraversal(node.left)
+//            output.append(contentsOf: left)
+//        }
+//        if node.right != nil {
+//            var right = preorderTraversal(node.right)
+//            output.append(contentsOf: right)
+//        }
+//
+//        return output
+//    }
+//}
+
+// Iteerative approach
 class Solution {
     func preorderTraversal(_ root: TreeNode?) -> [Int] {
         guard let node = root else {
@@ -19,15 +43,8 @@ class Solution {
         }
         
         var output = [Int]()
-        output.append(node.val)
-        if node.left != nil {
-            var left = preorderTraversal(node.left)
-            output.append(contentsOf: left)
-        }
-        if node.right != nil {
-            var right = preorderTraversal(node.right)
-            output.append(contentsOf: right)
-        }
+        
+        
         
         return output
     }
